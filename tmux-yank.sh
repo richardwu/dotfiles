79618800f0adf6b03eceb73b8a82b4cd2df4257a
Richard Wu <richardwu1997@gmail.com>
2018-12-02 12:37:07 -0500

    Fixed tmux config again.

--- tmux-yank.sh
+++ tmux-yank.sh
@@ -1,12 +1,16 @@
 # Resolve copy backend: pbcopy (OSX), reattach-to-user-namespace (OSX), xclip/xsel (Linux), or network service
 # get data either form stdin or from file
 
+is_app_installed() {
+  type "$1" &>/dev/null
+}
+
 buf=$(cat "$@")
 copy_backend=""
-if is_app_installed pbcopy; then
-  copy_backend="pbcopy"
-elif is_app_installed reattach-to-user-namespace; then
+if is_app_installed reattach-to-user-namespace; then
   copy_backend="reattach-to-user-namespace pbcopy"
+elif is_app_installed pbcopy; then
+  copy_backend="pbcopy"
 elif [ -n "${DISPLAY-}" ] && is_app_installed xsel; then
   copy_backend="xsel -i --clipboard"
 elif [ -n "${DISPLAY-}" ] && is_app_installed xclip; then
@@ -16,6 +20,7 @@ elif [ -n "${DISPLAY-}" ] && is_app_installed xclipboard; then
 elif [ "$(ss -n -4 state listening "( sport = 19988 )" | tail -n +2 | wc -l)" -eq 1 ]; then
   copy_backend="nc localhost 19988"
 fi
+
 # if copy backend is resolved, copy and exit
 if [ -n "$copy_backend" ]; then
   printf "$buf" | eval "$copy_backend"
