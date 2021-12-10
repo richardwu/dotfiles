6739f1426f70fbe9bbb09877989ac528b8b26aff
Richard Wu <richardwu1997@gmail.com>
2021-12-10 15:36:34 -0500

    Added homebrew fzf config.

--- .fzf.zsh
+++ .fzf.zsh
@@ -1,5 +1,24 @@
 # Setup fzf
 # ---------
+
+### Homebrew
+
+if [[ ! "$PATH" == */opt/homebrew/opt/fzf/bin* ]]; then
+  export PATH="${PATH:+${PATH}:}/opt/homebrew/opt/fzf/bin"
+fi
+
+# Auto-completion
+# ---------------
+[[ $- == *i* ]] && source "/opt/homebrew/opt/fzf/shell/completion.zsh" 2> /dev/null
+
+# Key bindings
+# ------------
+source "/opt/homebrew/opt/fzf/shell/key-bindings.zsh"
+
+
+### Non-homebrew
+
+# ---------
 if [[ ! "$PATH" == *$HOME/.fzf/bin* ]]; then
   export PATH="${PATH:+${PATH}:}$HOME/.fzf/bin"
 fi
