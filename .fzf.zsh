a27f6eb324bfe7239416bf9ba31c97e8d3f4c71e
Richard Wu <richardwu1997@gmail.com>
2022-05-07 20:07:32 -0400

    Updated fzf to work for both brew + custom.

--- .fzf.zsh
+++ .fzf.zsh
@@ -1,13 +1,19 @@
 # Setup fzf
 # ---------
-if [[ ! "$PATH" == */$HOME/.fzf/bin* ]]; then
-  export PATH="${PATH:+${PATH}:}/$HOME/.fzf/bin"
+if [[ ! "$PATH" == */usr/local/opt/fzf/bin* ]]; then
+  export PATH="${PATH:+${PATH}:}/usr/local/opt/fzf/bin"
+fi
+
+if [[ ! "$PATH" == *$HOME/.fzf/bin* ]]; then
+  export PATH="${PATH:+${PATH}:}${HOME}/.fzf/bin"
 fi
 
 # Auto-completion
 # ---------------
-[[ $- == *i* ]] && source "/$HOME/.fzf/shell/completion.zsh" 2> /dev/null
+[[ $- == *i* ]] && [[ -f "/usr/local/opt/fzf/shell/completion.zsh" ]] && source "/usr/local/opt/fzf/shell/completion.zsh" 2> /dev/null
+[[ $- == *i* ]] && [[ -f "$HOME/.fzf/shell/completion.zsh" ]] && source "$HOME/.fzf/shell/completion.zsh" 2> /dev/null
 
 # Key bindings
 # ------------
-[[ -f "/$HOME/.fzf/shell/key-bindings.zsh" ]] && source "/$HOME/.fzf/shell/key-bindings.zsh"
+[[ -f "/usr/local/opt/fzf/shell/key-bindings.zsh" ]] && source "/usr/local/opt/fzf/shell/key-bindings.zsh"
+[[ -f "$HOME/.fzf/shell/key-bindings.zsh" ]] && source "$HOME/.fzf/shell/key-bindings.zsh"
